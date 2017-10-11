a9a66a579be866166a3f776f537f33809f0ce864
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-11 17:36:49 +0200

    Dynamic property updates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
-	auth: AuthReducer //meaning: the 'auth' piece of state is produced by the AuthReducer
-});
+	auth: AuthReducer, //meaning: the 'auth' piece of state is produced by the AuthReducer
+	employeeForm: EmployeeFormReducer
+});	
