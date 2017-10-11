25b2f8f9accf5adae2e89423f8df1c25561e6721
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-11 11:45:10 +0200

    Navigation bar buttons

--- src/Router.js
+++ src/Router.js
@@ -11,7 +11,13 @@ const RouterComponent = () => {
 			</Scene>
 
 			<Scene key="main">
-				<Scene key="employeeList" component={EmployeeList} title="Employees" />
+				<Scene 
+					onRight={ }
+					rightTitle="Add"
+					key="employeeList" 
+					component={EmployeeList} 
+					title="Employees"
+				/>
 			</Scene>
 		</Router>
 	);
